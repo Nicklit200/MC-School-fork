e3a8e526807a96f3cb9222bde97475eb9d11d4f2
Nicklit200 <78360431+Nicklit200@users.noreply.github.com>
2026-09-16 21:29:20 +0200

    Use experimental final-answer homework flow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -29,7 +29,7 @@ import { MyCardsPage } from './pages/student/MyCardsPage';
 import { StudentCardsDetailPage } from './pages/student/StudentCardsDetailPage';
 import { StudentHomeworksListPage } from './pages/student/StudentHomeworksListPage';
 import { StudentHomeworkDetailPage } from './pages/student/StudentHomeworkDetailPage';
-import { PdfHomeworkWithSubmissionPage } from './pages/student/PdfHomeworkWithSubmissionPage';
+import { ExperimentalPdfHomeworkSubmissionPage } from './pages/student/ExperimentalPdfHomeworkSubmissionPage';
 import { SettingsPage } from './pages/student/SettingsPage';
 import { ParentPage } from './pages/parent/ParentPage';
 
@@ -71,7 +71,7 @@ export function App() {
       <Route path="/homeworks" element={<Navigate to="/student/homeworks" replace />} />
       <Route path="/student/homeworks" element={<ProtectedRoute role="STUDENT"><Layout><StudentHomeworksListPage /></Layout></ProtectedRoute>} />
       <Route path="/student/homeworks/:homeworkId" element={<ProtectedRoute role="STUDENT"><Layout><StudentHomeworkDetailPage /></Layout></ProtectedRoute>} />
-      <Route path="/student/homeworks/:homeworkId/worksheet" element={<ProtectedRoute role="STUDENT"><Layout><PdfHomeworkWithSubmissionPage /></Layout></ProtectedRoute>} />
+      <Route path="/student/homeworks/:homeworkId/worksheet" element={<ProtectedRoute role="STUDENT"><Layout><ExperimentalPdfHomeworkSubmissionPage /></Layout></ProtectedRoute>} />
       <Route path="/settings" element={<ProtectedRoute role="STUDENT"><Layout><SettingsPage /></Layout></ProtectedRoute>} />
 
       <Route path="/parent" element={<ProtectedRoute role="PARENT"><Layout><ParentPage /></Layout></ProtectedRoute>} />
