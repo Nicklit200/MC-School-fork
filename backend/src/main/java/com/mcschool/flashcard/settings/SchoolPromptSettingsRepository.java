4defe551e296785b62642a0eda9204e8cf2f0624
Nicklit200 <78360431+Nicklit200@users.noreply.github.com>
2026-09-17 10:57:48 +0200

    Add school prompt settings repository

---
+++ backend/src/main/java/com/mcschool/flashcard/settings/SchoolPromptSettingsRepository.java
@@ -0,0 +1,6 @@
+package com.mcschool.flashcard.settings;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolPromptSettingsRepository extends JpaRepository<SchoolPromptSettings, Short> {
+}
